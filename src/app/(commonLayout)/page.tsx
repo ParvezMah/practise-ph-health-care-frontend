be8eebb3715c35f705d6df0ed2d6d7e254924d5e
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-09 11:39:22 +0600

    66-1 Planning The Routing Architecture Of Ph Healthcare

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,19 @@
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
+
+
+// Pages Structure:
+// Pablic Pages : Home, About Us, Contact Us
+// Auth Pages : Login, Register, Forgot Password, Reset Password
+// Patient Pages : Dashboard, dashboard/my-profile, dashboard/book-appointment, dashboard/appointments, dashboard/prescriptions, dashboard/billing, dashboard/settings
+// Admin Dashboard : Patient Management, Doctor Management, Appointment Management, /admin/dashboard, /admin/dashbaord/patient-management, /admin/dashboard/doctor-management, /admin/dashboard/appointment-management
+// Doctor Dashboard : My Profile, Appointments, My Schedule, /doctor/dashboard, /doctor/my-profile, /doctor/appointments.
+// Error Pages : 404 Not Found, 500 Internal Server Error
