78bde61098d9aa3fcae195ae17698d3e527075d3
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-09 12:04:28 +0600

    66-4 Form Handling With Useactionstate Hook

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
+import { useActionState } from "react";
 import { Button } from "./ui/button";
 import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
 import { Input } from "./ui/input";
 
 const RegisterForm = () => {
+  const [state, formAction, isPending] = useActionState(
+    (currentState: any, formData: any) => {
+      console.log("currentState : ", currentState);
+      console.log("formData : ", formData.get("name"));
+      return { success: true };
+    },
+    null
+  );
   return (
-    <form>
+    <form action={formAction}>
       <FieldGroup>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {/* Name */}
